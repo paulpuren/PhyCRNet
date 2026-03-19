8c2338b878ceac0d02b5a72ccdd8fe5170befa65
Pu Ren <puren@Pus-MacBook-Pro-2.local>
2026-03-19 13:43:58 -0700

    refact FN solver

--- Datasets/FN_2d_solver_[HighOrder].py
+++ Datasets/FN_2d_solver_[HighOrder].py
@@ -1,194 +1,257 @@
-'''high-order FD solver for FN equation'''
-# spatial diff: 4th order laplacian
-# temporal diff: O(dt^5) due to RK4
+"""High-order finite-difference solver for the 2D FitzHugh-Nagumo equation.
 
-import scipy.io
-import numpy as np
+Spatial derivatives use a fourth-order Laplacian with periodic boundary
+conditions. Time integration uses classical RK4.
+"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Sequence, Tuple
+
 import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io
+
 
 np.random.seed(66)
 
-def apply_laplacian(mat, dx = 1.0):
-    # dx is inversely proportional to N
-    """This function applies a discretized Laplacian
-    in periodic boundary conditions to a matrix
-    For more information see 
-    https://en.wikipedia.org/wiki/Discrete_Laplace_operator#Implementation_via_operator_discretization
-    """
 
-    # the cell appears 4 times in the formula to compute
-    # the total difference
-    neigh_mat = -5*mat.copy()
+Array2D = np.ndarray
 
-    # Each direct neighbor on the lattice is counted in
-    # the discrete difference formula
-    neighbors = [ 
-                    ( 4/3,  (-1, 0) ),
-                    ( 4/3,  ( 0,-1) ),
-                    ( 4/3,  ( 0, 1) ),
-                    ( 4/3,  ( 1, 0) ),
-                    (-1/12,  (-2, 0)),
-                    (-1/12,  (0, -2)),
-                    (-1/12,  (0, 2)),
-                    (-1/12,  (2, 0)),
-                ]
-
-    # shift matrix according to demanded neighbors
-    # and add to this cell with corresponding weight
-    for weight, neigh in neighbors:
-        neigh_mat += weight * np.roll(mat, neigh, (0,1))
-
-    return neigh_mat/dx**2
+LAPLACIAN_STENCIL: Tuple[Tuple[float, Tuple[int, int]], ...] = (
+    (4.0 / 3.0, (-1, 0)),
+    (4.0 / 3.0, (0, -1)),
+    (4.0 / 3.0, (0, 1)),
+    (4.0 / 3.0, (1, 0)),
+    (-1.0 / 12.0, (-2, 0)),
+    (-1.0 / 12.0, (0, -2)),
+    (-1.0 / 12.0, (0, 2)),
+    (-1.0 / 12.0, (2, 0)),
+)
 
 
-def update_rk4(U0, V0, DU=1.0, DV=100.0, alpha=0.01, beta=0.25, dt=0.05, dx=1.0):
-    """Update with Runge-kutta-4 method
-       See https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta_methods
-    """
-    ############# Stage 1 ##############
-    # compute the diffusion part of the update
-    laplace_u = apply_laplacian(U0, dx)
-    laplace_v = apply_laplacian(V0, dx)
+@dataclass(frozen=True)
+class SimulationConfig:
+    """Configuration for FitzHugh-Nagumo data generation."""
 
-    u_t = DU * laplace_u + U0 - U0 ** 3 - V0 + alpha
-    v_t = DV * laplace_v + (U0 - V0) * beta
-
-    K1_u = u_t
-    K1_v = v_t
-
-    ############# Stage 2 ##############
-    U1 = U0 + K1_u * dt/2.0
-    V1 = V0 + K1_v * dt/2.0
-
-    laplace_u = apply_laplacian(U1, dx)
-    laplace_v = apply_laplacian(V1, dx)
-
-    u_t = DU * laplace_u + U1 - U1 ** 3 - V1 + alpha
-    v_t = DV * laplace_v + (U1 - V1) * beta
-
-    K2_u = u_t
-    K2_v = v_t
-
-    ############# Stage 3 ##############
-    U2 = U0 + K2_u * dt/2.0
-    V2 = V0 + K2_v * dt/2.0
-
-    laplace_u = apply_laplacian(U2, dx)
-    laplace_v = apply_laplacian(V2, dx)
-
-    u_t = DU * laplace_u + U2 - U2 ** 3 - V2 + alpha
-    v_t = DV * laplace_v + (U2 - V2) * beta
-
-    K3_u = u_t
-    K3_v = v_t
-
-    ############# Stage 4 ##############
-    U3 = U0 + K3_u * dt
-    V3 = V0 + K3_v * dt
-
-    laplace_u = apply_laplacian(U3, dx)
-    laplace_v = apply_laplacian(V3, dx)
-
-    u_t = DU * laplace_u + U3 - U3 ** 3 - V3 + alpha
-    v_t = DV * laplace_v + (U3 - V3) * beta
-
-    K4_u = u_t
-    K4_v = v_t
-
-    # Final solution
-    U = U0 + dt*(K1_u+2*K2_u+2*K3_u+K4_u)/6.0
-    V = V0 + dt*(K1_v+2*K2_v+2*K3_v+K4_v)/6.0
-
-    return U, V
+    diffusion_u: float = 1.0
+    diffusion_v: float = 100.0
+    alpha: float = 0.01
+    beta: float = 0.25
+    grid_height: int = 128
+    grid_width: int = 128
+    dt: float = 0.0020
+    dx: float = 1.0
+    n_simulation_steps: int = 10000
+    save_every: int = 10
+    preview_frames: int = 81
+    preview_stride: int = 10
+    figure_dir: Path = Path("./figures/2dFN")
+    data_dir: Path = Path("./data/2dFN")
+    output_name: str = "FN_1001x2x128x128.mat"
+    initial_condition_std: float = 0.05
+    random_seed: int = 66
 
 
-def get_initial_A_and_B(M, N):
-    """get the initial chemical concentrations"""
-    
-    A = np.random.normal(scale=0.05, size=(M, N)) 
-    B = np.random.normal(scale=0.05, size=(M, N))
-    
-    return A, B
+def apply_periodic_stencil(
+        mat: Array2D,
+        stencil: Sequence[Tuple[float, Tuple[int, int]]],
+        scale: float,
+        center_weight: float = 0.0,
+    ) -> Array2D:
+    """Apply a weighted stencil under periodic boundary conditions."""
+
+    result = center_weight * mat.copy()
+    for weight, shift in stencil:
+        result += weight * np.roll(mat, shift=shift, axis=(0, 1))
+    return result / scale
 
 
-def postProcess(output, xmin, xmax, ymin, ymax, num, fig_save_dir):
-    ''' num: Number of time step
-    '''
-    x = np.linspace(0, 128, 129)
-    x = x[:-1]
-    x_star, y_star = np.meshgrid(x, x)
+def apply_laplacian(mat: Array2D, dx: float = 1.0) -> Array2D:
+    """Return the fourth-order discrete Laplacian."""
 
-    u = output[num, 0, :, :]
-    v = output[num, 1, :, :]
+    return apply_periodic_stencil(
+        mat=mat,
+        stencil=LAPLACIAN_STENCIL,
+        scale=dx**2,
+        center_weight=-5.0,
+    )
 
-    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 7))
+
+def get_temporal_diff(
+        u: Array2D,
+        v: Array2D,
+        diffusion_u: float,
+        diffusion_v: float,
+        alpha: float,
+        beta: float,
+        dx: float,
+    ) -> Tuple[Array2D, Array2D]:
+    """Evaluate the FitzHugh-Nagumo PDE right-hand side."""
+
+    laplace_u = apply_laplacian(u, dx)
+    laplace_v = apply_laplacian(v, dx)
+
+    u_t = diffusion_u * laplace_u + u - u**3 - v + alpha
+    v_t = diffusion_v * laplace_v + (u - v) * beta
+    return u_t, v_t
+
+
+def update_rk4(
+        u0: Array2D,
+        v0: Array2D,
+        diffusion_u: float = 1.0,
+        diffusion_v: float = 100.0,
+        alpha: float = 0.01,
+        beta: float = 0.25,
+        dt: float = 0.05,
+        dx: float = 1.0,
+    ) -> Tuple[Array2D, Array2D]:
+    """Advance one time step using RK4."""
+
+    k1_u, k1_v = get_temporal_diff(u0, v0, diffusion_u, diffusion_v, alpha, beta, dx)
+
+    u1 = u0 + 0.5 * dt * k1_u
+    v1 = v0 + 0.5 * dt * k1_v
+    k2_u, k2_v = get_temporal_diff(u1, v1, diffusion_u, diffusion_v, alpha, beta, dx)
+
+    u2 = u0 + 0.5 * dt * k2_u
+    v2 = v0 + 0.5 * dt * k2_v
+    k3_u, k3_v = get_temporal_diff(u2, v2, diffusion_u, diffusion_v, alpha, beta, dx)
+
+    u3 = u0 + dt * k3_u
+    v3 = v0 + dt * k3_v
+    k4_u, k4_v = get_temporal_diff(u3, v3, diffusion_u, diffusion_v, alpha, beta, dx)
+
+    u = u0 + (dt / 6.0) * (k1_u + 2.0 * k2_u + 2.0 * k3_u + k4_u)
+    v = v0 + (dt / 6.0) * (k1_v + 2.0 * k2_v + 2.0 * k3_v + k4_v)
+    return u, v
+
+
+def sample_initial_conditions(config: SimulationConfig) -> Tuple[Array2D, Array2D]:
+    """Sample the initial chemical concentrations."""
+
+    shape = (config.grid_height, config.grid_width)
+    u = np.random.normal(scale=config.initial_condition_std, size=shape)
+    v = np.random.normal(scale=config.initial_condition_std, size=shape)
+    return u, v
+
+
+def build_plot_grid(resolution: int) -> Tuple[Array2D, Array2D]:
+    """Build a cell-centered plotting grid."""
+
+    coordinates = np.arange(resolution)
+    return np.meshgrid(coordinates, coordinates)
+
+
+def post_process(
+    output: np.ndarray,
+    xmin: float,
+    xmax: float,
+    ymin: float,
+    ymax: float,
+    step_index: int,
+    fig_save_dir: Path,
+) -> None:
+    """Render the saved u/v fields for a specific frame."""
+
+    resolution = output.shape[-1]
+    x_star, y_star = build_plot_grid(resolution)
+    u = output[step_index, 0, :, :]
+    v = output[step_index, 1, :, :]
+
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(7, 7))
     fig.subplots_adjust(hspace=0.3, wspace=0.3)
 
-    cf = ax[0].scatter(x_star, y_star, c=u, alpha=0.95, edgecolors='none', cmap='RdYlBu', 
-           marker='s', s=3)
-    ax[0].axis('square')
-    ax[0].set_xlim([xmin, xmax])
-    ax[0].set_ylim([ymin, ymax])
-    cf.cmap.set_under('black')
-    cf.cmap.set_over('whitesmoke')
-    ax[0].set_title('u-FDM')
-    fig.colorbar(cf, ax=ax[0, 0], fraction=0.046, pad=0.04)
+    for axis, field, title in zip(axes, (u, v), ("u-FDM", "v-FDM")):
+        color_mesh = axis.scatter(
+            x_star,
+            y_star,
+            c=field,
+            alpha=0.95,
+            edgecolors="none",
+            cmap="RdYlBu",
+            marker="s",
+            s=3,
+        )
+        axis.axis("square")
+        axis.set_xlim([xmin, xmax])
+        axis.set_ylim([ymin, ymax])
+        color_mesh.cmap.set_under("black")
+        color_mesh.cmap.set_over("whitesmoke")
+        axis.set_title(title)
+        fig.colorbar(color_mesh, ax=axis, fraction=0.046, pad=0.04)
 
-    cf = ax[1].scatter(x_star, y_star, c=v, alpha=0.95, edgecolors='none', cmap='RdYlBu', 
-           marker='s', s=3)
-    ax[1].axis('square')
-    ax[1].set_xlim([xmin, xmax])
-    ax[1].set_ylim([ymin, ymax])
-    cf.cmap.set_under('black')
-    cf.cmap.set_over('whitesmoke')
-    ax[1].set_title('v-FDM')
-    fig.colorbar(cf, ax=ax[1, 0], fraction=0.046, pad=0.04)
-
-    # plt.draw()
-    plt.savefig(fig_save_dir + '/uv_[i=%d].png'%(num))
-    plt.close('all')
-
-if __name__ == '__main__':
-    # Diffusion coefficients
-    DA = 1
-    DB = 100
-    
-    # reaction coeff
-    alpha = 0.01
-    beta = 0.25
-    
-    # grid size
-    M = 128
-    N = 128
-    delta_t = 0.0020             # 0.002 for RK4 = 0.001 for Euler
-    dx = 1
-    
-    # intialize the chemical concentrations, random_incluence=0
-    A, B = get_initial_A_and_B(M, N)
-    A_record = A.copy()[None,...]
-    B_record = B.copy()[None,...]
-    
-    N_simulation_steps = 10000
-    for step in range(N_simulation_steps):
-        
-        # RK4
-        A, B = update_rk4(A, B, DU=1.0, DV=100.0, alpha=0.01, beta=0.25, dt=delta_t, dx=1.0)
-    
-        # Save every 0.02s
-        if (step+1) % 10 == 0:
-            print(step, '\n')
-            A_record = np.concatenate((A_record, A[None,...]), axis=0)
-            B_record = np.concatenate((B_record, B[None,...]), axis=0)
-    
-    UV = np.concatenate((A_record[None,...], B_record[None,...]), axis=0)
-    UV = np.transpose(UV, [1, 0, 2, 3])
-    
-    fig_save_dir = './figures/2dFN/'
-    for i in range(0,81):
-        postProcess(UV, 0, 128, 0, 128, num=10*i, fig_save_dir=fig_save_dir)
-    
-    # save data
-    data_save_dir = './data/2dFN/'
-    scipy.io.savemat(data_save_dir + 'FN_1001x2x128x128.mat', {'uv': UV})
+    fig.savefig(fig_save_dir / f"uv_[i={step_index}].png")
+    plt.close(fig)
 
 
+def run_simulation(config: SimulationConfig) -> np.ndarray:
+    """Run the FN simulation and return a tensor shaped as (t, c, h, w)."""
+
+    u, v = sample_initial_conditions(config)
+    u_records = [u.copy()]
+    v_records = [v.copy()]
+
+    for step in range(config.n_simulation_steps):
+        u, v = update_rk4(
+            u,
+            v,
+            diffusion_u=config.diffusion_u,
+            diffusion_v=config.diffusion_v,
+            alpha=config.alpha,
+            beta=config.beta,
+            dt=config.dt,
+            dx=config.dx,
+        )
+
+        if (step + 1) % config.save_every == 0:
+            print(f"Completed step {step + 1}/{config.n_simulation_steps}")
+            u_records.append(u.copy())
+            v_records.append(v.copy())
+
+    uv = np.stack((np.stack(u_records, axis=0), np.stack(v_records, axis=0)), axis=0)
+    return np.transpose(uv, (1, 0, 2, 3))
+
+
+def create_preview_images(output: np.ndarray, config: SimulationConfig) -> None:
+    """Generate preview figures for a subset of saved frames."""
+
+    config.figure_dir.mkdir(parents=True, exist_ok=True)
+    for frame_index in range(config.preview_frames):
+        saved_step = frame_index * config.preview_stride
+        if saved_step >= output.shape[0]:
+            break
+        post_process(
+            output=output,
+            xmin=0,
+            xmax=config.grid_width,
+            ymin=0,
+            ymax=config.grid_height,
+            step_index=saved_step,
+            fig_save_dir=config.figure_dir,
+        )
+
+
+def save_dataset(output: np.ndarray, config: SimulationConfig) -> Path:
+    """Persist the generated trajectory as a MAT file."""
+
+    config.data_dir.mkdir(parents=True, exist_ok=True)
+    output_path = config.data_dir / config.output_name
+    scipy.io.savemat(output_path, {"uv": output})
+    return output_path
+
+
+def main() -> None:
+    """Generate FN simulation data and preview figures."""
+
+    config = SimulationConfig()
+    output = run_simulation(config)
+    create_preview_images(output, config)
+    save_dataset(output, config)
+
+
+if __name__ == "__main__":
+    main()
